7f7f3a88d4656b9b58a26f1ba1cb1a3060843236
Renato Crivano <renato.crivano@gmail.com>
2023-04-13 16:01:09 -0300

    Compatibilizando com SwaggerServlet v2.0.5 e publicando v1.2.0

---
+++ src/main/java/br/jus/trf2/assijus/system/api/AssijusSystemContext.java
@@ -0,0 +1,7 @@
+package br.jus.trf2.assijus.system.api;
+
+import com.crivano.swaggerservlet.ISwaggerApiContext;
+
+public interface AssijusSystemContext extends ISwaggerApiContext {
+
+}
--- src/main/java/br/jus/trf2/assijus/system/api/IAssijusSystem.java
+++ src/main/java/br/jus/trf2/assijus/system/api/IAssijusSystem.java
@@ -1,6 +1,7 @@
 package br.jus.trf2.assijus.system.api;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ import com.crivano.swaggerservlet.ISwaggerResponse;
 import com.crivano.swaggerservlet.ISwaggerResponseFile;
 
 public interface IAssijusSystem {
-	public class Document implements ISwaggerModel {
+	public static class Document implements ISwaggerModel {
 		public String id;
 		public String secret;
 		public String code;
@@ -22,161 +23,162 @@ public interface IAssijusSystem {
 		public String extra;
 	}
 
-	public class Signature implements ISwaggerModel {
+	public static class Signature implements ISwaggerModel {
 		public String ref;
 		public String signer;
 		public String kind;
 	}
 
-	public class Movement implements ISwaggerModel {
+	public static class Movement implements ISwaggerModel {
 		public Date time;
 		public String department;
 		public String kind;
 	}
 
-	public class Warning implements ISwaggerModel {
+	public static class Warning implements ISwaggerModel {
 		public String label;
 		public String description;
 	}
 
-	public class Error implements ISwaggerModel {
+	public static class Error implements ISwaggerModel {
 		public String error;
 	}
 
-	public class DocListGetRequest implements ISwaggerRequest {
-		public String cpf;
-		public String urlapi;
-	}
-
-	public class DocListGetResponse implements ISwaggerResponse {
-		public List<Document> list;
-	}
-
 	public interface IDocListGet extends ISwaggerMethod {
-		public void run(DocListGetRequest req, DocListGetResponse resp) throws Exception;
-	}
-
-	public class DocIdPdfGetRequest implements ISwaggerRequest {
-		public String id;
-		public String cpf;
-	}
-
-	public class DocIdPdfGetResponse implements ISwaggerResponse, ISwaggerResponseFile {
-		public String contenttype = "application/pdf";
-		public String contentdisposition = "attachment";
-		public Long contentlength;
-		public InputStream inputstream;
-		public Map<String, List<String>> headerFields;
-
-		public String getContenttype() {
-			return contenttype;
+		public static class Request implements ISwaggerRequest {
+			public String cpf;
+			public String urlapi;
 		}
 
-		public void setContenttype(String contenttype) {
-			this.contenttype = contenttype;
+		public static class Response implements ISwaggerResponse {
+			public List<Document> list = new ArrayList<>();
 		}
 
-		public String getContentdisposition() {
-			return contentdisposition;
-		}
-
-		public void setContentdisposition(String contentdisposition) {
-			this.contentdisposition = contentdisposition;
-		}
-
-		public Long getContentlength() {
-			return contentlength;
-		}
-
-		public void setContentlength(Long contentlength) {
-			this.contentlength = contentlength;
-		}
-
-		public InputStream getInputstream() {
-			return inputstream;
-		}
-
-		public void setInputstream(InputStream inputstream) {
-			this.inputstream = inputstream;
-		}
-
-		public Map<String, List<String>> getHeaderFields() {
-			return headerFields;
-		}
-
-		public void setHeaderFields(Map<String, List<String>> headerFields) {
-			this.headerFields = headerFields;
-		}
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 	public interface IDocIdPdfGet extends ISwaggerMethod {
-		public void run(DocIdPdfGetRequest req, DocIdPdfGetResponse resp) throws Exception;
-	}
+		public static class Request implements ISwaggerRequest {
+			public String id;
+			public String cpf;
+		}
 
-	public class DocIdHashGetRequest implements ISwaggerRequest {
-		public String id;
-		public String cpf;
-		public String extra;
-	}
+		public static class Response implements ISwaggerResponse, ISwaggerResponseFile {
+			public String contenttype = "application/pdf";
+			public String contentdisposition = "attachment";
+			public Long contentlength;
+			public InputStream inputstream;
+			public Map<String, List<String>> headerFields;
+			public String getContenttype() {
+				return contenttype;
+			}
 
-	public class DocIdHashGetResponse implements ISwaggerResponse {
-		public byte[] sha1;
-		public byte[] sha256;
-		public String policy;
-		public String extra;
-		public byte[] doc;
-		public String secret;
+			public void setContenttype(String contenttype) {
+				this.contenttype = contenttype;
+			}
+
+			public String getContentdisposition() {
+				return contentdisposition;
+			}
+
+			public void setContentdisposition(String contentdisposition) {
+				this.contentdisposition = contentdisposition;
+			}
+
+			public Long getContentlength() {
+				return contentlength;
+			}
+
+			public void setContentlength(Long contentlength) {
+				this.contentlength = contentlength;
+			}
+
+			public InputStream getInputstream() {
+				return inputstream;
+			}
+
+			public void setInputstream(InputStream inputstream) {
+				this.inputstream = inputstream;
+			}
+
+			public Map<String, List<String>> getHeaderFields() {
+				return headerFields;
+			}
+
+			public void setHeaderFields(Map<String, List<String>> headerFields) {
+				this.headerFields = headerFields;
+			}
+
+		}
+
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 	public interface IDocIdHashGet extends ISwaggerMethod {
-		public void run(DocIdHashGetRequest req, DocIdHashGetResponse resp) throws Exception;
-	}
+		public static class Request implements ISwaggerRequest {
+			public String id;
+			public String cpf;
+			public String extra;
+		}
 
-	public class DocIdSignPutRequest implements ISwaggerRequest {
-		public String id;
-		public String cpf;
-		public String name;
-		public Date time;
-		public byte[] sha1;
-		public String extra;
-		public byte[] envelope;
-	}
+		public static class Response implements ISwaggerResponse {
+			public byte[] sha1;
+			public byte[] sha256;
+			public String policy;
+			public String extra;
+			public byte[] doc;
+			public String secret;
+		}
 
-	public class DocIdSignPutResponse implements ISwaggerResponse {
-		public String status;
-		public List<Warning> warning;
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 	public interface IDocIdSignPut extends ISwaggerMethod {
-		public void run(DocIdSignPutRequest req, DocIdSignPutResponse resp) throws Exception;
-	}
+		public static class Request implements ISwaggerRequest {
+			public String id;
+			public String cpf;
+			public String name;
+			public Date time;
+			public byte[] sha1;
+			public String extra;
+			public byte[] envelope;
+		}
 
-	public class DocIdInfoGetRequest implements ISwaggerRequest {
-		public String id;
-	}
+		public static class Response implements ISwaggerResponse {
+			public String status;
+			public List<Warning> warning = new ArrayList<>();
+		}
 
-	public class DocIdInfoGetResponse implements ISwaggerResponse {
-		public String status;
-		public List<Signature> signature;
-		public List<Movement> movement;
-		public String secret;
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 	public interface IDocIdInfoGet extends ISwaggerMethod {
-		public void run(DocIdInfoGetRequest req, DocIdInfoGetResponse resp) throws Exception;
-	}
+		public static class Request implements ISwaggerRequest {
+			public String id;
+		}
 
-	public class SignRefGetRequest implements ISwaggerRequest {
-		public String ref;
-	}
+		public static class Response implements ISwaggerResponse {
+			public String status;
+			public List<Signature> signature = new ArrayList<>();
+			public List<Movement> movement = new ArrayList<>();
+			public String secret;
+		}
 
-	public class SignRefGetResponse implements ISwaggerResponse {
-		public byte[] envelope;
-		public Date time;
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 	public interface ISignRefGet extends ISwaggerMethod {
-		public void run(SignRefGetRequest req, SignRefGetResponse resp) throws Exception;
+		public static class Request implements ISwaggerRequest {
+			public String ref;
+		}
+
+		public static class Response implements ISwaggerResponse {
+			public byte[] envelope;
+			public Date time;
+		}
+
+		public void run(Request req, Response resp, AssijusSystemContext ctx) throws Exception;
 	}
 
 }
+
